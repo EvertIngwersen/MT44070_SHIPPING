768fafe6712cef7c05589b420baf14c3a4a2727a
Evert Ingwersen <everti2000@gmail.com>
2025-03-06 12:58:50 +0100

    import data

--- vessel_excel_reader.py
+++ vessel_excel_reader.py
@@ -6,3 +6,7 @@ Created on Thu Mar  6 12:42:53 2025
 """
 
 import pandas as pd
+
+path = r"C:\Users\evert\Documents\TU-Delft\TIL Master\MT44070 Shipping Management\MT44070_REPO\MT44070_SHIPPING\Vessels_DATA\MODEL_23964.xlsx"
+df = pd.read_excel(path, sheet_name="CostShip") 
+
