4d1568bbfdf59038c83b6ff06802b778146cafd9
Evert Ingwersen <everti2000@gmail.com>
2025-03-10 15:48:11 +0100

    added poem

---
+++ poem.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 10 15:47:10 2025
+
+@author: evert
+"""
+
+# The River of Time
+
+while True:
+    breathe()
+    if dreams and not fears:
+        chase(dreams)
+    elif dusk and not hope:
+        rest()
+    else:
+        wander()
+    
+    if heart.is_full() or soul.is_tired():
+        break
